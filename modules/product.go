859d42b06e8d069fd390ae7203e20051f13b611c
ADMIN\Admin <drake2k9454@gmail.com>
2023-08-10 09:43:05 +0700

    add product models

--- modules/product.go
+++ modules/product.go
@@ -1,1 +1,15 @@
 package modules
+
+type Product struct {
+	ID           string        `gqlgen:"id"`
+	Manufacturer *Manufacturer `gqlgen:"manufacturer"`
+	Upc          string        `gqlgen:"upc"`
+	Name         string        `gqlgen:"name"`
+	Price        int           `gqlgen:"price"`
+}
+
+type Manufacturer struct {
+	ID   string `gqlgen:"id"`
+	Name string `gqlgen:"name"`
+}
+
